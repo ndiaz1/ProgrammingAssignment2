9b346342a02ae9498b405e2743b3c19596b8c31a
Nicolas Diaz <ndiaz41720@gmail.com>
2014-08-24 20:10:36 +0200

    Assignment 2 submission

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,53 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+#Overall objective:
+#The functions below create a method to circumvent the calculation of the inverse of 
+#a matrix by allocating its value and simply retrieving it when needed calculated. This essentially circumvents 
+#the need to repeat a potentially time-consuming calculation. 
+
+#"makeCacheMatrix" description:
+#The primary purpose of the makeCacheMatrix function is to take in an invertible matrix (x) and attach allocation (setinverse) and recall (get and getinverse) functions in order to allow 
+#subsequent functions to call the (x) matrix and different computations derived from it. In this case, the makeCacheMatrix is concerned with 
+#the computation of the inverse of the (x) matrix. This makeCacheMatrix makes use the superassignement command and
+#the lexical scoping that R implements to allocate values outside of its nested functions in order to pass them through other functions.
+
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        y<<-x
+        inverse<-NULL
+        
+        get<-function(){y}
+        
+        setinverse<-function(solve)
+          {inverse<<-solve}
+        
+        getinverse<-function(){inverse}
+        
+        list(get=get,setinverse=setinverse,getinverse=getinverse)
 }
 
 
-## Write a short comment describing this function
+# "cacheSolve" description:
+# The following function uses the allocation functions in makeCacheMatrix to call any previously tagged (x) matrix
+# inverse value. In the event of no inverse value is retrieved from the makeCacheMatrix function, a value is computed
+# and set into allocated memory. This allocated value is retrieved in subsequent calls of the cacheSolve function and 
+# prevents unnecessary repetition of the matix inverse calculation.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  
+        inverse<-x$getinverse()
+        
+        if(!is.null(inverse)){
+          message("getting cached data")
+          return(inverse)
+        }
+        data<-x$get()
+        
+        inverse<-solve(y, ...)
+        
+        x$setinverse(inverse)
+        
+        inverse
+        
 }
